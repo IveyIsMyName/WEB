a5b11d585d03738c928f0053d4a77508bff7323c
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2025-07-22 14:42:14 -0400

    DOM intro

--- 02-JavaScript/03-DOM/DOM.js
+++ 02-JavaScript/03-DOM/DOM.js
@@ -1,4 +1,11 @@
-//JavaScript source code
+﻿//JavaScript source code
 let check = document.getElementById("check")
 check.innerHTML = "Hello DOM";
 console.log(check.outerHTML);
+
+let configuration = document.getElementById("configuration");
+configuration.append("append()");
+configuration.prepend("prepend()");
+configuration.before("before()");
+configuration.after("after()");
+
